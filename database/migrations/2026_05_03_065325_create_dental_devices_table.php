ac50820de729690fb17517fcc4f157ec2e6556d6
shahinsamiur <shahinsamiur@yahoo.com>
2026-05-03 14:14:32 +0600

    feat (dental_devices) : dental_devices table create

--- database/migrations/2026_05_03_065325_create_dental_devices_table.php
+++ database/migrations/2026_05_03_065325_create_dental_devices_table.php
@@ -6,20 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('dental_devices', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary()->default(DB::raw('(UUID())'));
+            $table->uuid('patient_id');
+            $table->foreign('patient_id')
+                  ->references('id')
+                  ->on('patient_profiles')
+                  ->onDelete('cascade');
+            $table->string('device_type', 100);
+            $table->text('other_device')->nullable();
+            $table->string('tooth_type', 50)->nullable();
+            $table->enum('jaw_position', [
+                'upper_left',
+                'upper_right',
+                'lower_left',
+                'lower_right'
+            ])->nullable();
+            $table->date('placement_date')->nullable();
+            $table->string('facility_name', 255)->nullable();
+            $table->string('dentist_name', 255)->nullable();
+            $table->json('documents')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('dental_devices');
