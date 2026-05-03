f7273d1787db28535bb9c4a812ac7ae4cc630bee
shahinsamiur <shahinsamiur@yahoo.com>
2026-05-03 14:08:42 +0600

    feat (physical_activities) : physical_activities table create

--- database/migrations/2026_05_03_065324_create_physical_activities_table.php
+++ database/migrations/2026_05_03_065324_create_physical_activities_table.php
@@ -6,20 +6,53 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('physical_activities', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary()->default(DB::raw('(UUID())'));
+            $table->uuid('patient_id');
+            $table->foreign('patient_id')
+                  ->references('id')
+                  ->on('patient_profiles')
+                  ->onDelete('cascade');
+            $table->string('activity_name', 100);
+            $table->enum('category', [
+                'cardio',
+                'fitness',
+                'wellbeing',
+                'team_sport',
+                'other'
+            ]);
+            $table->text('other_activity')->nullable();
+            $table->enum('frequency', [
+                'daily',
+                'occasional',
+                'regular',
+                'frequent'
+            ])->nullable();
+            $table->enum('avg_duration', [
+                '15min',
+                '30min',
+                '45min',
+                '1hr',
+                '+1hr'
+            ])->nullable();
+            $table->enum('intensity', [
+                'low',
+                'moderate',
+                'high'
+            ])->nullable();
+            $table->enum('objective', [
+                'fitness',
+                'weight_loss',
+                'rehabilitation',
+                'competitive',
+                'wellbeing'
+            ])->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('physical_activities');
