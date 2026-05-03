003ce378ed5767b375b010354e94d3e287381f2c
shahinsamiur <shahinsamiur@yahoo.com>
2026-05-03 14:10:41 +0600

    feat (subtance_consumption) : substance_consumption table create

--- database/migrations/2026_05_03_065324_create_substance_consumption_table.php
+++ database/migrations/2026_05_03_065324_create_substance_consumption_table.php
@@ -6,20 +6,40 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('substance_consumption', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary()->default(DB::raw('(UUID())'));
+            $table->uuid('patient_id');
+            $table->foreign('patient_id')
+                  ->references('id')
+                  ->on('patient_profiles')
+                  ->onDelete('cascade');
+            $table->string('substance_name', 100);
+            $table->text('other_substance')->nullable();
+            $table->enum('frequency', [
+                'occasional',
+                'monthly',
+                'weekly',
+                'daily'
+            ])->nullable();
+            $table->enum('mode', [
+                'inhalation',
+                'oral',
+                'injection',
+                'other'
+            ])->nullable();
+            $table->date('last_consumption')->nullable();
+            $table->enum('context', [
+                'recreational',
+                'misused_medical',
+                'festive',
+                'other'
+            ])->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('substance_consumption');
