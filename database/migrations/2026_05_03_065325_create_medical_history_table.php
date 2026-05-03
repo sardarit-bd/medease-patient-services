1c023be1c0c43c9539b2dcb15f0b20412b622c3c
shahinsamiur <shahinsamiur@yahoo.com>
2026-05-03 14:12:21 +0600

    feat (medical_history) : medical_history table create

--- database/migrations/2026_05_03_065325_create_medical_history_table.php
+++ database/migrations/2026_05_03_065325_create_medical_history_table.php
@@ -6,20 +6,35 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('medical_history', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary()->default(DB::raw('(UUID())'));
+            $table->uuid('patient_id');
+            $table->foreign('patient_id')
+                  ->references('id')
+                  ->on('patient_profiles')
+                  ->onDelete('cascade');
+            $table->enum('history_type', [
+                'medical',
+                'surgical'
+            ]);
+            $table->string('category', 100)->nullable();
+            $table->string('condition_name', 255);
+            $table->text('other_condition')->nullable();
+            $table->date('event_date')->nullable();
+            $table->enum('date_range', [
+                '<1yr',
+                '1-5yr',
+                '>5yr'
+            ])->nullable();
+            $table->uuid('facility_id')->nullable();
+            $table->string('facility_name', 255)->nullable();
+            $table->text('notes')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('medical_history');
