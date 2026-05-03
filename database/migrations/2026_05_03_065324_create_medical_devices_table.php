269df2f0520a63b7d2dbbd0f11a94eb016e53d3f
shahinsamiur <shahinsamiur@yahoo.com>
2026-05-03 14:07:02 +0600

    feat (medical_devices) : user medical devices table create

--- database/migrations/2026_05_03_065324_create_medical_devices_table.php
+++ database/migrations/2026_05_03_065324_create_medical_devices_table.php
@@ -6,20 +6,38 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('medical_devices', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary()->default(DB::raw('(UUID())'));
+            $table->uuid('patient_id');
+            $table->foreign('patient_id')
+                  ->references('id')
+                  ->on('patient_profiles')
+                  ->onDelete('cascade');
+            $table->string('device_type', 100);
+            $table->string('device_name', 255)->nullable();
+            $table->text('other_device')->nullable();
+            $table->string('body_location', 100)->nullable();
+            $table->date('implant_date')->nullable();
+            $table->enum('date_range', [
+                '<1yr',
+                '1-5yr',
+                '>5yr',
+                'unknown'
+            ])->nullable();
+            $table->uuid('facility_id')->nullable();
+            $table->enum('follow_up', [
+                'yes',
+                'no',
+                'pending'
+            ])->nullable();
+            $table->json('documents')->nullable();
+            $table->text('notes')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('medical_devices');
