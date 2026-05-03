ce255f8c02d67f65f028a6fa36716c91741fa0fa
shahinsamiur <shahinsamiur@yahoo.com>
2026-05-03 14:16:12 +0600

    feat (vaccination_records) : vaccination_records  table create

--- database/migrations/2026_05_03_065326_create_vaccination_records_table.php
+++ database/migrations/2026_05_03_065326_create_vaccination_records_table.php
@@ -6,20 +6,38 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('vaccination_records', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary()->default(DB::raw('(UUID())'));
+            $table->uuid('patient_id');
+            $table->foreign('patient_id')
+                  ->references('id')
+                  ->on('patient_profiles')
+                  ->onDelete('cascade');
+            $table->string('vaccine_name', 255);
+            $table->enum('vaccine_category', [
+                'mandatory',
+                'recommended',
+                'specific',
+                'other'
+            ])->nullable();
+            $table->text('other_vaccine')->nullable();
+            $table->enum('dose_type', [
+                'first',
+                'second',
+                'booster',
+                'complete_schedule'
+            ])->nullable();
+            $table->date('vaccination_date')->nullable();
+            $table->string('professional_name', 255)->nullable();
+            $table->string('facility_name', 255)->nullable();
+            $table->string('batch_number', 100)->nullable();
+            $table->json('documents')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('vaccination_records');
