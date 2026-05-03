868692b5d344bb9a54426bacd09c01c4a1a11e46
shahinsamiur <shahinsamiur@yahoo.com>
2026-05-03 14:17:01 +0600

    feat (allergies) : allergies  table create

--- database/migrations/2026_05_03_065326_create_allergies_table.php
+++ database/migrations/2026_05_03_065326_create_allergies_table.php
@@ -6,20 +6,41 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('allergies', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary()->default(DB::raw('(UUID())'));
+            $table->uuid('patient_id');
+            $table->foreign('patient_id')
+                  ->references('id')
+                  ->on('patient_profiles')
+                  ->onDelete('cascade');
+            $table->enum('allergy_type', [
+                'drug',
+                'food',
+                'environmental',
+                'contact_skin',
+                'venom_insect'
+            ]);
+            $table->string('substance_name', 255);
+            $table->text('other_substance')->nullable();
+            $table->json('reaction_types')->nullable();
+            $table->enum('severity', [
+                'mild',
+                'moderate',
+                'severe'
+            ])->nullable();
+            $table->date('reaction_date')->nullable();
+            $table->enum('is_confirmed', [
+                'confirmed',
+                'suspected',
+                'not_confirmed'
+            ])->nullable();
+            $table->json('emergency_treatment')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('allergies');
