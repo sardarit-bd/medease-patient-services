c0f0b67813661510773c7b3c7a6be146d04de75e
shahinsamiur <shahinsamiur@yahoo.com>
2026-05-03 14:13:47 +0600

    feat (transfusion_history) : transfusion_history table create

--- database/migrations/2026_05_03_065325_create_transfusion_history_table.php
+++ database/migrations/2026_05_03_065325_create_transfusion_history_table.php
@@ -6,20 +6,41 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('transfusion_history', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary()->default(DB::raw('(UUID())'));
+            $table->uuid('patient_id');
+            $table->foreign('patient_id')
+                  ->references('id')
+                  ->on('patient_profiles')
+                  ->onDelete('cascade');
+            $table->date('transfusion_date')->nullable();
+            $table->string('facility_name', 255)->nullable();
+            $table->enum('department', [
+                'emergencies',
+                'operating_room',
+                'intensive_care',
+                'surgery',
+                'hematology',
+                'maternity',
+                'other'
+            ])->nullable();
+            $table->integer('units_count')->nullable();
+            $table->enum('reason', [
+                'surgery',
+                'hemorrhage',
+                'accident',
+                'hematological_disease',
+                'childbirth',
+                'other'
+            ])->nullable();
+            $table->text('other_reason')->nullable();
+            $table->text('notes')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('transfusion_history');
