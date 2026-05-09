650d152da1ed4f32db58ff952325271312ae1201
shahinsamiur <shahinsamiur@yahoo.com>
2026-05-09 16:24:35 +0600

    fix : folder name fixed

--- app/Modules/inventory/Routes/api.php
+++ app/Modules/inventory/Routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Modules\inventory\Controllers\InventoryController;
 use Illuminate\Support\Facades\Route;
-
+// hello
 Route::middleware('auth:sanctum')->prefix('inventory')->group(function () {
 
     Route::get('/stock', [InventoryController::class, 'stock']);
